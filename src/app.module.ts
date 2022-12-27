a13395ebe63f7396d12fbb349dc62870a053e45b
AbdulazizBozorov <abdulazizheir@gmail.com>
2022-12-27 11:27:01 +0500

    Bot is workin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { EchoModule } from './echo/echo.module';
+import { GreeterModule } from './greeter/greeter.module';
+import { GreeterBotName } from './app.constants';
+import { sessionMiddleware } from './middleware/session.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TelegrafModule.forRoot({
+      token: '5982233794:AAFq9GKdrbidL6630SkY1k9cILT-wX0Hnlw',
+      include: [EchoModule],
+    }),
+    TelegrafModule.forRootAsync({
+      botName: GreeterBotName,
+      useFactory: () => ({
+        token: '5623718105:AAHdEgjEqLIrnSAmZR07mbKRpZjQ1g72ldo',
+        middlewares: [sessionMiddleware],
+        include: [GreeterModule],
+      }),
+    }),
+    EchoModule,
+    GreeterModule,
+  ],
 })
 export class AppModule {}
